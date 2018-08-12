a7c2fca6e1a134ddbf860866e74af68c8ecdb007
rcs021 <cadastrorcs@gmail.com>
2018-08-12 13:11:15 -0300

    Adicionando Seção de Carrosel com fotos

--- js/arquivo.js
+++ js/arquivo.js
@@ -1,12 +1,8 @@
-function enviar() {
-     
-    var nome = document.getElementById("txt-nome");
-    
-    if (nome.value !== "") {
-        
-        alert("Obrigado " + nome.value + ", mas está em desenvolvimento");
-    }
-    else {
-        alert("Obrigado, mas esta área está em desenvolvimento")
-    }
+function enviar(){  
+    var nome = document.getElementById("txt-nome");  
+        if(nome.value != ""){
+            alert("Obrigado " + nome.value + ", mas está em desenvolvimento");
+        } else {
+          alert("Obrigado Zé, mas esta área está em desenvolvimento");
+        }
 }
